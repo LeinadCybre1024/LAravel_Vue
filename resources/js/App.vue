57fc82819bffb64fc4c118ba76806bd7e73dd065
LeinadCybre1024 <leinadcybre@gmail.com>
2023-06-27 10:08:12 +0300

    added laravel sail to docker container

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <h1> Hello, Vuejs with Laravel </h1>
+  <h1> Count : {{ count }}</h1>
+  <button @click="count++">
++
+  </button>
+  <button @click="count--">
+    -
+  </button>
 </template>
 <script>
+import { ref } from 'vue'
 export default {
   setup() {
+    const count = ref(0)
+
+    return{
+        count
+    }
 
    }
 }
